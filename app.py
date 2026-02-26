067859752082381bc61ec689141732a359b56d03
camivelandia04 <camilovelandia.r@gmail.com>
2026-02-26 13:59:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Conversión de Texto a Audio")
+st.title("Asistente de lectura para estudiantes")
 image = Image.open('gato_raton.png')
 st.image(image, width=350)
 with st.sidebar:
@@ -20,12 +20,7 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.write('¡Aqui puedes poner tus textos o articulos académicos y yo los pasaré a audio para que puedas esuccharlos mientras vas al trabajo, a la u o mientras haces otras actividades'
         
         )
            
